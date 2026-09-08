02fc353b525ef4d7c2cb43efe102d3261ed2d695
Gabriel Perez <gperezm@profesores.uniajc.edu.co>
2026-09-07 21:05:35 -0500

    Agregando codigo fuente carpeta src primer commit

---
+++ src/main/java/com/uniajc/Main.java
@@ -0,0 +1,12 @@
+package com.uniajc;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world desde grupo 411!");
+        System.out.println("La suma de -2 y -6 es: " + sumar(-2, -6));
+    }
+
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+}
