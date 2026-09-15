d00d9a02c5460ebd4c324be4936dbc2581dec5aa
Gabriel Perez <gperezm@profesores.uniajc.edu.co>
2026-09-14 21:34:33 -0500

    Agregando info del ejercicio

--- src/main/java/com/uniajc/Main.java
+++ src/main/java/com/uniajc/Main.java
@@ -12,6 +12,9 @@ public class Main {
         Persona persona2 = new Persona("123456789");
         persona2.mostrarInformacion();
 
+        persona2.setIdentificacion("324234");
+        persona2.getIdentificacion();
+
         // Crear un objeto de la clase Persona con parámetros
         Persona persona3 = new Persona("987654321", "Juan Pérez", 30, "correo@mail.com");
         persona3.mostrarInformacion();
